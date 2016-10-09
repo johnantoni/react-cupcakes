8444af507b838669ba08fd2d479cd42c295d64dc
Michelle Clark <michelle@uxgent.co>
2016-10-09 00:40:16 -0400

    preloader.

--- src/components/CupcakeImage.js
+++ src/components/CupcakeImage.js
@@ -1,6 +1,26 @@
 import React from 'react';
 
 class CupcakeImage extends React.Component {
+  constructor() {
+    super();
+    {/*getInitialState if using ES6*/}
+    this.state = {
+      images: ["blackberry.png", "blueberry.png", "cherry.png",
+            "chocolate-baubles.png", "chocolate-cake.png", "chocolate-icing.png", "cream-cheese-icing.png",
+            "kiwi.png", "mint-cream-icing.png", "peanut-butter-icing.png", "pink-buttercream-icing.png",
+            "rainbow-sprinkles.png", "raspberry.png", "red-and-white-stars.png", "red-baubles.png",
+            "red-velvet-cake.png", "reeses.png", "smarties.png", "strawberry.png", "vanilla-cake.png"]
+    };
+  }
+
+  // Preload images at the earliest possible lifecycle event
+  componentWillMount() {
+    this.state.images.forEach((src) => {
+      const img = document.createElement('img');
+      img.src = src; // Assigning the img src immediately requests the image
+    });
+  }
+
   componentDidMount() {
     this.updateCanvas();
   }
@@ -11,7 +31,7 @@ class CupcakeImage extends React.Component {
 
   loadBackground(ctx) {
     var background = new Image();
-    background.src = "assets/background.png"
+    background.src = "background.png"
     ctx.drawImage(background, 0, 0);
   }
 
@@ -25,15 +45,22 @@ class CupcakeImage extends React.Component {
 
   addImage(ctx, imgSrc) {
     var image = new Image();
-    image.src = `assets/${imgSrc}`
+    image.src = `${imgSrc}`
+
+    this.state.images.map((img) => {
+      return `<img src=${img.src}></img>`
+    })
+
     image.onload = function()  {
       ctx.drawImage(image, 0, 0);
     }
   }
 
-  updateCanvas(cakeImgSrc, icingImgSrc) {
+  updateCanvas(cakeImgSrc, icingImgSrc, loadedimages) {
     let cakeSrc = this.props.cakeImgSrc;
     let icingSrc = this.props.icingImgSrc;
+
+    console.log(loadedimages);
 
     const ctx = this.refs.canvas.getContext('2d');
     this.loadBackground(ctx);
